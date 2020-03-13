d749bb15db6b13996f89cac2346e05652c6c38ab
Touraj Vaziri <touraj@vrsquare.ca>
2020-03-13 12:08:48 +0100

    Moved the config file to src folder

--- src/protractor.conf.js
+++ src/protractor.conf.js
@@ -2,10 +2,10 @@ exports.config = {
     framework: 'jasmine', //Type of Framework used 
     directConnect:false,
     seleniumAddress: 'http://localhost:4444/wd/hub',
-    specs: ['./src/home-spec.js'], //Name of the Specfile
+    specs: ['../src/home-spec.js'], //Name of the Specfile
     onPrepare() { 
           require('ts-node').register({ 
-          project: require('path').join(__dirname, './tsconfig.json') // Relative path of tsconfig.json file 
+          project: require('path').join(__dirname, '../tsconfig.json') // Relative path of tsconfig.json file 
         });
     } 
 }
