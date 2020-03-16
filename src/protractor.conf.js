5cf13a506180019d6630f37f85aba184dab56564
Touraj Vaziri <touraj@vrsquare.ca>
2020-03-16 09:59:04 +0100

    Fixing the test project.

--- src/protractor.conf.js
+++ src/protractor.conf.js
@@ -1,11 +1,21 @@
 exports.config = {
     framework: 'jasmine', //Type of Framework used 
-    directConnect:false,
-    seleniumAddress: 'http://localhost:4444/wd/hub',
+    directConnect:true, // Set to false if you like to run Selenium server manually
+   // seleniumAddress: 'http://localhost:4444/wd/hub',
     specs: ['../src/home-spec.js'], //Name of the Specfile
+
+    capabilites: {
+        'browserName': 'chrome'
+    },
+
     onPrepare() { 
           require('ts-node').register({ 
           project: require('path').join(__dirname, '../tsconfig.json') // Relative path of tsconfig.json file 
         });
-    } 
+    },
+
+    // Check what this is
+    jasmineNodeOpts: {
+        defaultTimeoutInterval: 30000
+    }
 }
