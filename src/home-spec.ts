1600607e9eb85ccf2008f5d9c04fa9b13b43f2dc
Touraj Vaziri <touraj@vrsquare.ca>
2020-03-16 12:14:06 +0100

    Adding some options for interactive element navigation.

--- src/home-spec.ts
+++ src/home-spec.ts
@@ -1,8 +1,10 @@
-import {browser} from 'protractor';
+import { browser } from 'protractor';
 describe('Browser Navigation', () => {
    it('Exampe of back method', () => {
       browser.get('https://material.angularjs.org')
-      .then(()=>(browser.get('https://www.protractortest.org/#/faq'))
-      .then(() => browser.pause()))
+         .then(() => (browser.get('https://www.protractortest.org/#/faq'))
+            .then(() => browser.explore())) // Use for interaction with elements intractively
+      // .then(() => browser.debugger()) // Use when debugging before and element not found  
+      // .then(() => browser.pause())) // Use when debugging before and element not found  (YOU THIS ONE Recommended)
    });
 });
--- src/protractor.conf.js
+++ src/protractor.conf.js
@@ -1,21 +1,23 @@
 exports.config = {
     framework: 'jasmine', //Type of Framework used 
-    directConnect:true, // Set to false if you like to run Selenium server manually
-   // seleniumAddress: 'http://localhost:4444/wd/hub',
-    specs: ['../src/home-spec.js'], //Name of the Specfile
+    directConnect: true, // Set to false if you like to run Selenium server manually
+    // seleniumAddress: 'http://localhost:4444/wd/hub',
+    specs: ['../dist/out-tsc/home-spec.js'], //Name of the Specfile
 
     capabilites: {
         'browserName': 'chrome'
     },
 
-    onPrepare() { 
-          require('ts-node').register({ 
-          project: require('path').join(__dirname, '../tsconfig.json') // Relative path of tsconfig.json file 
+    onPrepare() {
+        require('ts-node').register({
+            project: require('path').join(__dirname, '../tsconfig.json') // Relative path of tsconfig.json file 
         });
     },
 
     // Check what this is
     jasmineNodeOpts: {
         defaultTimeoutInterval: 30000
-    }
+    },
+
+    baseUrl: 'https://material.angularjs.org'  // Use for interaction with elements intractively
 }
