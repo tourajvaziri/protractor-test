d7995b01075865fe149ef5ad27b0b6baa65fed64
Touraj Vaziri <touraj@vrsquare.ca>
2020-03-21 21:11:51 +0100

    Adding reports to test project.

--- src/protractor.conf.js
+++ src/protractor.conf.js
@@ -1,8 +1,8 @@
 var HtmlScreenshotReporter = require('protractor-jasmine2-screenshot-reporter');
 
 var reporter = new HtmlScreenshotReporter({
-  dest: 'target/screenshots',
-  filename: 'my-report.html',
+  dest: 'test_results/screenshotsReporter',
+  filename: 'testsReport.html',
   reportOnlyFailedSpecs: true,
   captureOnlyFailedSpecs: true
 });
@@ -26,14 +26,45 @@ exports.config = {
 
   onPrepare() {
     jasmine.getEnv().addReporter(reporter);
+
     require('ts-node').register({
       project: require('path').join(__dirname, '../tsconfig.json') // Relative path of tsconfig.json file 
     });
 
     var AllureReporter = require('jasmine-allure-reporter');
     jasmine.getEnv().addReporter(new AllureReporter({
-      resultsDir: 'target/allure'
+      resultsDir: 'test_results/allureReporter'
     }));
+
+    var jasmineReporters = require('jasmine-reporters');
+    jasmine.getEnv().addReporter(new jasmineReporters.JUnitXmlReporter({
+      consolidateAll: true,
+      savePath: 'test_results/htmlReporter/',
+      filePrefix: 'xmlresults'
+    }));
+
+    var fs = require('fs-extra');
+
+    fs.emptyDir('test_results/htmlReporter/screenshots/', function (err) {
+      console.log(err);
+    });
+
+    jasmine.getEnv().addReporter({
+      specDone: function (result) {
+        if (result.status == 'failed') {
+          browser.getCapabilities().then(function (caps) {
+            var browserName = caps.get('browserName');
+
+            browser.takeScreenshot().then(function (png) {
+              var stream = fs.createWriteStream('test_results/htmlReporter/screenshots/' + browserName + '-' + result.fullName + '.png');
+              stream.write(new Buffer(png, 'base64'));
+              stream.end();
+            });
+          });
+        }
+      }
+    });
+
   },
 
   // Check what this is
@@ -45,6 +76,32 @@ exports.config = {
   afterLaunch: function (exitCode) {
     return new Promise(function (resolve) {
       reporter.afterLaunch(resolve.bind(this, exitCode));
+    });
+  },
+
+  onComplete: function () {
+    var browserName, browserVersion;
+    var capsPromise = browser.getCapabilities();
+
+    capsPromise.then(function (caps) {
+      browserName = caps.get('browserName');
+      browserVersion = caps.get('version');
+      platform = caps.get('platform');
+
+      var HTMLReport = require('protractor-html-reporter-2');
+
+      testConfig = {
+        reportTitle: 'Protractor Test Execution Report',
+        outputPath: 'test_results/htmlReporter/',
+        outputFilename: 'ProtractorTestReport',
+        screenshotPath: './screenshots',
+        testBrowser: browserName,
+        browserVersion: browserVersion,
+        modifiedSuiteName: false,
+        screenshotsOnlyOnFailure: true,
+        testPlatform: platform
+      };
+      new HTMLReport().from('test_results/htmlReporter/xmlresults.xml', testConfig);
     });
   },
 
